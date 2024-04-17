d16cfde26e1ceb299a911d6c46fd9ba04ba16300
Artur Deters <arturdamotta@gmail.com>
2024-04-17 18:11:30 -0300

    adicionando temas

---
+++ Client/src/themes/themes.js
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+export const defaultDark = createTheme({
+    palette:{
+        mode: 'dark',
+        primary: {
+            main: '#5d1c8b',
+            light: '#7666e0',
+            dark: '#20155f'
+        }
+    }
+})
+
+export const defaultLight = createTheme({
+    palette:{
+        mode: 'light',
+        primary: {
+            main: '#5d1c8b',
+            light: '#7666e0',
+            dark: '#20155f'
+        }
+    }
+})
