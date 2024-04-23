f8e9a36511b56698317256d07e932d75a0516a6d
Artur Deters <arturdamotta@gmail.com>
2024-04-23 11:02:23 -0300

    ola

--- Client/src/components/AppBar/AppBar.jsx
+++ Client/src/components/AppBar/AppBar.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import { styled, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import MuiDrawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
