fca46f9f5ebf61cb305e31a43960149a70404850
Artur Deters <arturdamotta@gmail.com>
2024-06-28 16:16:59 -0300

    integrando a tela de put das turmas com o backend

--- Client/src/pages/principalPages/turmas/EditarTurmas/EditarTurmas.jsx
+++ Client/src/pages/principalPages/turmas/EditarTurmas/EditarTurmas.jsx
@@ -7,48 +7,102 @@ import {
   Button,
 } from "@mui/material";
 import { defaultDark } from "../../../../themes/themes";
-import { useNavigate } from "react-router-dom";
+import { redirect, useNavigate, useParams } from "react-router-dom";
 import classes from "../styles/EditarTurmas";
+import { useEffect, useState } from "react";
+import GetDataTurmasById from "../core/GetDataTurmasById";
+import axios from "axios";
 
+// eslint-disable-next-line react-hooks/exhaustive-deps
 
 const EditarTurmas = () => {
+  const [data, setData] = useState({});
+  const { id } = useParams();
 
-  const navigate = useNavigate()
+  useEffect(() => {
+    const fetchData = async () => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      try {
+        const result = await GetDataTurmasById(id);
+        setData(...result);
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const navigate = useNavigate();
 
   const saveAndRedirect = () => {
-    navigate('/turmas')
-  }
+    axios
+      .put("http://localhost:3030/turmas/editar/"+id, {
+        nome: data.nome,
+        ano_inicio: data.ano_inicio
+      })
+      .then((response) => {
+        console.log(response);
+        navigate("/turmas");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    
+  };
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setData((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
 
   return (
     <ThemeProvider theme={defaultDark}>
       <CssBaseline />
-      <Box
-        sx={classes.principalBox}
-      >
-        <Box
-          sx={classes.topCard}
-        >
+      <Box sx={classes.principalBox}>
+        <Box sx={classes.topCard}>
           <Typography variant="h3">Editar turmas</Typography>
         </Box>
-        <Box
-          sx={classes.boxComponents}
-        >
+        <Box sx={classes.boxComponents}>
           <Box sx={classes.typographyBox}>
-            <Typography marginBottom="20px" variant="h4">Altere os parametros desejados</Typography>
+            <Typography marginBottom="20px" variant="h4">
+              Altere os parâmetros desejados
+            </Typography>
             <Typography variant="body1">
               Lembre-se de salvar antes de sair da página
             </Typography>
           </Box>
           <Box sx={classes.boxCentralize}>
-            <Box
-              sx={classes.textFieldBox}
-            >
-              <TextField id="name" label="Nome" variant="filled" />
-              <TextField id="ano" label="Ano de início" variant="filled" />
+            <Box sx={classes.textFieldBox}>
+              <TextField
+                id="nome"
+                label="Nome"
+                variant="filled"
+                value={data.nome}
+                onChange={handleChange}
+              />
+              <TextField
+                id="ano_inicio"
+                label="Ano de início"
+                variant="filled"
+                value={data.ano_inicio}
+                onChange={handleChange}
+              />
             </Box>
             <Box sx={classes.buttonBox}>
-              <Button sx={{ marginRight: '20px'}} size="large" variant="contained" onClick={saveAndRedirect}>‎ ‎ ‎ Salvar ‎ ‎ ‎ </Button>
-              <Button color="error" size="large" variant="contained">
+              <Button
+                sx={{ marginRight: "20px" }}
+                size="large"
+                variant="contained"
+                onClick={saveAndRedirect}
+              >
+                ‎ ‎ ‎ Salvar ‎ ‎ ‎{" "}
+              </Button>
+              <Button color="error" size="large" variant="contained" onClick={() => navigate("/turmas")}>
                 Cancelar
               </Button>
             </Box>
--- Client/src/pages/principalPages/turmas/Turmas.jsx
+++ Client/src/pages/principalPages/turmas/Turmas.jsx
@@ -1,11 +1,18 @@
-import { Box, Button, Typography, Paper, CssBaseline, ThemeProvider } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  Paper,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
 
 // import SearchIcon from "@mui/icons-material/Search";
 import SearchBar from "../../../components/UI/SearchBar/SearchBar.jsx";
 import { useEffect, useState } from "react";
 
 import UiAppBar from "../../../components/AppBar/AppBar";
-import { defaultDark} from "../../../themes/themes";
+import { defaultDark } from "../../../themes/themes";
 
 import { useNavigate } from "react-router-dom";
 import getDataTurmas from "./core/GetDataTurmas.jsx";
@@ -13,14 +20,10 @@ import classes from "./styles/Turmas.js";
 
 // ? Styles #########################
 
-
-
-
-
 const Turmas = () => {
   const [dataTurmas, setDataTurmas] = useState([]);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,9 +38,9 @@ const Turmas = () => {
     fetchData();
 
     console.log(typeof dataTurmas, "data turmas");
-    console.log( dataTurmas, "turmas");
-    
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    console.log(dataTurmas, "turmas");
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function redirect(id) {
@@ -45,42 +48,46 @@ const Turmas = () => {
   }
 
   function redirectCreateTurma() {
-    navigate('create')
+    navigate("create");
   }
 
   return (
-
     <ThemeProvider theme={defaultDark}>
       <CssBaseline />
       <UiAppBar>
-      <SearchBar>
-        <Button variant="contained" sx={classes.buttonAddTurma} onClick={redirectCreateTurma}>
-          Adicionar Turma
-        </Button>
-        <Button variant="contained">Agendar Conselho </Button>
-      </SearchBar>
-      <Box sx={classes.boxTurmas}>
-        {dataTurmas.map((turma) => (
-          <Paper
-            key={turma.id_turma}
-            elevation={8}
-            sx={classes.paperTurma}
+        <SearchBar>
+          <Button
+            variant="contained"
+            sx={classes.buttonAddTurma}
+            onClick={redirectCreateTurma}
           >
-            <Box sx={classes.flex1}>
-              <Typography variant="h4" mb={5}>
-                {turma.nome}
-              </Typography>
-              <Typography variant="body1">{turma.ano_inicio} Ano</Typography>
-            </Box>
-            <Box
-              sx={classes.buttonBoxPaper}
-            >
-              <Button variant="contained" onClick={() => redirect(turma.id_turma)}>Editar</Button>
-              <Button variant="contained">Notas</Button>  
-            </Box>
-          </Paper>
-        ))}
-      </Box>
+            Adicionar Turma
+          </Button>
+          <Button variant="contained">Agendar Conselho </Button>
+        </SearchBar>
+        <Box sx={classes.boxTurmas}>
+          {dataTurmas.map((turma) => (
+            <Paper key={turma.id_turma} elevation={8} sx={classes.paperTurma}>
+              <Box sx={classes.flex1}>
+                <Typography variant="h4" mb={5}>
+                  {turma.nome}
+                </Typography>
+                <Typography variant="body1">{turma.ano_inicio} Ano</Typography>
+              </Box>
+              <Box sx={classes.buttonBoxPaper}>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    redirect(turma.id_turma);
+                  }}
+                >
+                  Editar
+                </Button>
+                <Button variant="contained">Notas</Button>
+              </Box>
+            </Paper>
+          ))}
+        </Box>
       </UiAppBar>
     </ThemeProvider>
   );
