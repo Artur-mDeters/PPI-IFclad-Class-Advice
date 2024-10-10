79679576b6a0f2f2d8623dde9054ebd0fe8f13b7
Artur Deters <114366624+Artur-mDeters@users.noreply.github.com>
2024-10-10 13:54:29 -0300

    segundo teste do vscode legal

--- Client/src/pages/principalPages/config/Configuracoes.jsx
+++ Client/src/pages/principalPages/config/Configuracoes.jsx
@@ -3,13 +3,20 @@ import UiAppBar from "../../../components/AppBar/AppBar";
 import SearchBar from "../../../components/UI/SearchBar/SearchBar";
 // import { useState } from "react";
 // import { defaultDark, defaultLight } from "../../../themes/themes";
-
+import classes from "./classes";
 const Configuracoes = () => {
   // const [theme, setTheme] = useState(defaultDark)
 
   return (
-    <UiAppBar>
-      <SearchBar></SearchBar>
+    <UiAppBar sx={classes.principalUiAppBar}>
+      <SearchBar>
+        <Box> 
+          ajsd
+          asda
+          wd
+          asd
+        </Box>
+      </SearchBar>
     </UiAppBar>
   );
 };
---
+++ Client/src/pages/principalPages/config/classes.js
@@ -0,0 +1,10 @@
+const classes = {
+    principalUiAppBar: {
+        backgroundColor: 'black',
+    },
+    principalBox: {
+        backgroundColor: 'black',
+    }
+}
+
+export default classes
