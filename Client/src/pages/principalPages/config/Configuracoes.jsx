899b3d39148cadd63d7b9a5ba62931b586126f73
Artur Deters <arturdamotta@gmail.com>
2024-11-28 14:41:28 -0300

    começando a funcionalidade de themas

--- Client/src/pages/principalPages/config/Configuracoes.jsx
+++ Client/src/pages/principalPages/config/Configuracoes.jsx
@@ -1,16 +1,15 @@
 import UiAppBar from "../../../components/AppBar/AppBar";
 // import { Box, Button } from "@mui/material";
 import SearchBar from "../../../components/UI/SearchBar/SearchBar";
-// import { useState } from "react";
-// import { defaultDark, defaultLight } from "../../../themes/themes";
+
+import ThemeSelector from "../../../themes/ThemeSelector"; 
 
 const Configuracoes = () => {
-  // const [theme, setTheme] = useState(defaultDark)
 
   return (
     <UiAppBar title={"Configurações"}>
       <SearchBar></SearchBar>
-      {/* children  */}
+      {/* <ThemeSelector /> */}
     </UiAppBar>
   );
 };
---
+++ Client/src/themes/ThemeSelector.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { MenuItem, Select, InputLabel, FormControl } from "@mui/material";
+import {
+  defaultDark,
+  defaultLight,
+  greenDark,
+  greenLight,
+  blueDark,
+  yellowDark,
+  purpleLight,
+  orangeDark,
+  pinkLight,
+  cyanDark,
+  tealLight,
+  indigoDark,
+  grayDarkTheme,
+  highContrast,
+} from "./themes";
+import { useThemeContext } from "./Themecontext";
+
+const themes = {
+  defaultDark,
+  defaultLight,
+  greenDark,
+  greenLight,
+  blueDark,
+  yellowDark,
+  purpleLight,
+  orangeDark,
+  pinkLight,
+  cyanDark,
+  tealLight,
+  indigoDark,
+  grayDarkTheme,
+  highContrast,
+};
+
+const ThemeSelector = () => {
+  const { changeTheme } = useThemeContext();
+  const [selectedTheme, setSelectedTheme] = React.useState("defaultDark");
+
+  const handleThemeChange = (event) => {
+    const newThemeKey = event.target.value;
+    setSelectedTheme(newThemeKey);
+    changeTheme(themes[newThemeKey]);
+  };
+
+  return (
+    <FormControl fullWidth style={{ marginBottom: "20px" }}>
+      <InputLabel id="theme-select-label">Escolha um tema</InputLabel>
+      <Select
+        labelId="theme-select-label"
+        id="theme-select"
+        value={selectedTheme}
+        onChange={handleThemeChange}
+        label="Escolha um tema"
+      >
+        {Object.keys(themes).map((themeKey) => (
+          <MenuItem key={themeKey} value={themeKey}>
+            {themeKey}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default ThemeSelector;
